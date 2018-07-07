cbd6ec2736f6ff786d5158fa5c6cf915672f3989
seancarroll <seanc28@gmail.com>
2018-07-07 12:55:32 -0500

    clean up PollingStreamStoreNotifier

--- jsqlstreamstore-core/src/main/java/jsqlstreamstore/subscriptions/PollingStreamStoreNotifier.java
+++ jsqlstreamstore-core/src/main/java/jsqlstreamstore/subscriptions/PollingStreamStoreNotifier.java
@@ -14,56 +14,55 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
+/**
+ * An implementation of {@link StreamStoreNotifier} that polls the target stream store for new message.
+ */
 public class PollingStreamStoreNotifier implements StreamStoreNotifier {
     private static final Logger LOG = LoggerFactory.getLogger("PollingStreamStoreNotifier");
 
     private final Supplier<Long> _readHeadPosition;
+    // TODO: not sure if this is the best way
     private final PublishSubject<Long> _storeAppended = PublishSubject.create();
     private final Observable<Long> _appended;
 
     // https://www.nurkiewicz.com/2017/09/fixed-rate-vs-fixed-delay-rxjava-faq.html
     // https://github.com/ReactiveX/RxJava/issues/448
 
+    /**
+     *
+     * @param readonlyStreamStore
+     */
     public PollingStreamStoreNotifier(IReadOnlyStreamStore readonlyStreamStore) {
-        this(readonlyStreamStore, 1000, Executors.newSingleThreadScheduledExecutor());
+        this(() -> readonlyStreamStore.readHeadPosition(), 1000);
     }
 
-    public PollingStreamStoreNotifier(IReadOnlyStreamStore readonlyStreamStore, long interval, ScheduledExecutorService executorService) {
-        this(() -> readonlyStreamStore.readHeadPosition(), interval);
-    }
-
+    /**
+     *
+     * @param readHeadPosition
+     * @param interval
+     */
     public PollingStreamStoreNotifier(Supplier<Long> readHeadPosition, long interval) {
-        this._readHeadPosition = readHeadPosition;
-//        this._appended = Observable.fromCallable(() -> poll())
-//            .interval(interval, TimeUnit.MILLISECONDS, Schedulers.newThread());
-        //this._appended = Observable.fromCallable(() -> poll()).repeatWhen(o -> o.concatMap(v -> Observable.timer(interval, TimeUnit.MILLISECONDS, Schedulers.newThread())));
-        this._appended = Observable.interval(interval, TimeUnit.MILLISECONDS, Schedulers.newThread()).map(tick -> poll());
-        this._appended.subscribe(_storeAppended);
-        
-//        Observable.interval(interval, TimeUnit.MILLISECONDS, Schedulers.newThread()).map(tick -> poll()).subscribe(System.out::println);
-//        _storeAppended.interval(interval, TimeUnit.MILLISECONDS, Schedulers.newThread()).map(tick -> poll());
-//        _storeAppended.interval(interval, TimeUnit.MILLISECONDS, Schedulers.newThread()).repeatWhen(o -> o.concatMap(v -> Observable.timer(interval, TimeUnit.MILLISECONDS, Schedulers.newThread())));
+        this(readHeadPosition, interval, Schedulers.newThread());
     }
 
+    /**
+     *
+     * @param readHeadPosition
+     * @param interval
+     * @param scheduler
+     */
     public PollingStreamStoreNotifier(Supplier<Long> readHeadPosition, long interval, Scheduler scheduler) {
         this._readHeadPosition = readHeadPosition;
         this._appended = Observable.interval(interval, TimeUnit.MILLISECONDS, scheduler).map(tick -> poll());
-
-//        Schedulers.newThread().schedulePeriodicallyDirect({
-//            observer.onNext("application-state-from-network");
-//        }, 0, 1000, TimeUnit.MILLISECONDS);
+        this._appended.subscribe(_storeAppended);
     }
-
 
     @Override
     public void subscribe(Observer<? super Long> observer) {
         _storeAppended.subscribe(observer);
-//        _appended.subscribe(observer);
     }
 
-
-    // TODO: turn readHeadPosition to a supplier or a rxjava task
-    // TODO: should this return something?
+    // TODO probably doesnt need to return a Long
     private Long poll() {
         long headPosition = -1;
         long previousHeadPosition = headPosition;
@@ -78,9 +77,6 @@ public class PollingStreamStoreNotifier implements StreamStoreNotifier {
             if (headPosition > previousHeadPosition) {
                 _storeAppended.onNext(headPosition);
                 previousHeadPosition = headPosition;
-            } else {
-
-                // await Task.Delay(_interval, _disposed.Token);
             }
             return previousHeadPosition;
         }
--- jsqlstreamstore-postgres/src/test/java/jsqlstreamstore/PollingStreamStoreNotifierTests.java
+++ jsqlstreamstore-postgres/src/test/java/jsqlstreamstore/PollingStreamStoreNotifierTests.java
@@ -1,50 +1,18 @@
 package jsqlstreamstore;
 
-import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.observers.TestObserver;
-import io.reactivex.schedulers.Schedulers;
-import io.reactivex.schedulers.TestScheduler;
-import io.reactivex.subjects.PublishSubject;
-import io.reactivex.subscribers.TestSubscriber;
 import jsqlstreamstore.subscriptions.PollingStreamStoreNotifier;
-import org.flywaydb.core.Flyway;
-import org.junit.Before;
 import org.junit.Test;
-import org.reactivestreams.Subscriber;
-import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres;
 
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Supplier;
 
 import static org.junit.Assert.assertEquals;
-import static ru.yandex.qatools.embed.postgresql.EmbeddedPostgres.cachedRuntimeConfig;
 
 public class PollingStreamStoreNotifierTests {
-
-    // TODO: move this to some fixture or something
-    private EmbeddedPostgres postgres;
-    private PostgresStreamStore store;
-
-    @Before
-    public void setUp() throws Exception {
-        postgres = new EmbeddedPostgres();
-        String url = postgres.start(cachedRuntimeConfig(Paths.get(System.getProperty("java.io.tmpdir"), "pgembed")));
-
-        PostgresStreamStoreSettings settings = new PostgresStreamStoreSettings.Builder(url).build();
-
-        store = new PostgresStreamStore(settings);
-
-        Flyway flyway = new Flyway();
-        flyway.setDataSource(url, EmbeddedPostgres.DEFAULT_USER, EmbeddedPostgres.DEFAULT_PASSWORD);
-        flyway.setLocations("classpath:db/migrations");
-        flyway.migrate();
-    }
 
     @Test
     public void whenExceptionOccursReadingHeadPositionThenPollingShouldStillContinue() {
