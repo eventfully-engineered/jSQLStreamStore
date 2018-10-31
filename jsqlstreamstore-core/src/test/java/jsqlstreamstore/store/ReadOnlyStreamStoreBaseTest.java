d9437b46664c88aa857686574f7724669b2c622c
seancarroll <seanc28@gmail.com>
2018-10-31 07:10:10 -0500

    more junit5 test upgrades

--- jsqlstreamstore-core/src/test/java/jsqlstreamstore/store/ReadOnlyStreamStoreBaseTest.java
+++ jsqlstreamstore-core/src/test/java/jsqlstreamstore/store/ReadOnlyStreamStoreBaseTest.java
@@ -1,5 +1,34 @@
-import static org.junit.Assert.*;
+package jsqlstreamstore.store;
+
+import org.junit.jupiter.api.Test;
 
 public class ReadOnlyStreamStoreBaseTest {
+
+    @Test
+    public void shouldThrowWhenDisposedAndReadOccurs() {
+//        using(var fixture = GetFixture())
+//        {
+//            var store = await fixture.GetStreamStore();
+//            store.Dispose();
+//
+//            Func<Task> act = () => store.ReadAllForwards(Position.Start, 10);
+//
+//            act.ShouldThrow<ObjectDisposedException>();
+//        }
+    }
+
+    @Test
+    public void canDisposeMoreThanOnce() {
+//        using (var fixture = GetFixture())
+//        {
+//            var store = await fixture.GetStreamStore();
+//            store.Dispose();
+//
+//            Action act = store.Dispose;
+//
+//            act.ShouldNotThrow();
+//        }
+    }
+
 
 }
