d30ea573d05859f94175cec5b7aa8ed5d21a8cc7
seancarroll <seanc28@gmail.com>
2020-04-04 20:59:48 -0500

    swap varialbes for ? in sqlite scripts

--- jsqlstreamstore-sqlite/src/main/resources/db/scripts/ReadStreamForward.sql
+++ jsqlstreamstore-sqlite/src/main/resources/db/scripts/ReadStreamForward.sql
@@ -9,7 +9,7 @@ SELECT
 FROM messages
 INNER JOIN streams ON messages.stream_id = streams.id
 WHERE
-    messages.stream_id = _stream_id
-    AND messages.version >= stream_version
+    messages.stream_id = ?
+    AND messages.version >= ?
 ORDER BY messages.position
-LIMIT batch_size;
+LIMIT ?;
